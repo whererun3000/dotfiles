9a143875561686044adea50d67906288cc2fc5b1
whererun3000 <pookkljk151@gmail.com>
2024-01-26 00:39:51 +0800

    rm some file generated by zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,12 @@
+export ZSH_CACHE_HOME=$HOME/.cache/zsh
+
 export EDITOR=vi
 export VISUAL=vi
+
+export HISTFILE=$ZSH_CACHE_HOME/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=10000
+export ZSH_SESSION_FILE=$ZSH_CACHE_HOME/.zsh_session
 
 export USRLCL=/usr/local
 export GOROOT=/opt/go
