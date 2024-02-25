971980586dbe4933e7b1b2028d1e3a04d3124529
wrl <wrl@SK-20230509GLVS>
2024-02-25 15:18:16 +0800

    zsh/env.sh $GOBIN

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -11,4 +11,5 @@ export ZSH_SESSION_FILE=$ZSH_CACHE_HOME/.zsh_session
 export USRLCL=/usr/local
 export GOROOT=/opt/go
 export GOPATH=~/gopath
-export PATH=$PATH:$USRLCL/bin:$GOROOT/bin:$GOPATH/bin
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$USRLCL/bin:$GOROOT/bin:$GOBIN
