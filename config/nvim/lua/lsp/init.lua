a1f0947f5e5c411bbed6976f709571cd51908366
whererun3000 <pookkljk151@gmail.com>
2025-11-18 22:49:46 +0800

    config

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -1,5 +1,4 @@
 vim.lsp.enable(vim.g.enable_lsp)
-vim.lsp.inlay_hint.enable()
 vim.lsp.on_type_formatting.enable()
 
 vim.keymap.del('n', 'gO')
@@ -14,13 +13,13 @@ local fzf = require("fzf-lua")
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(args)
 		vim.keymap.set('n', 'gQ', vim.lsp.buf.format, { buf = args.buffer })
-		vim.keymap.set('n', 'gN', vim.lsp.buf.rename, { buf = args.buffer })
-		vim.keymap.set('n', 'gD', vim.lsp.buf.definition, { buf = args.buffer })
+		vim.keymap.set('n', 'gn', vim.lsp.buf.rename, { buf = args.buffer })
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buf = args.buffer })
 
-		vim.keymap.set('n', 'gR', fzf.lsp_references, { buf = args.buffer })
-		vim.keymap.set('n', 'gA', fzf.lsp_code_actions, { buf = args.buffer })
+		vim.keymap.set('n', 'gr', fzf.lsp_references, { buf = args.buffer })
+		vim.keymap.set('n', 'ga', fzf.lsp_code_actions, { buf = args.buffer })
 		vim.keymap.set('n', 'gI', fzf.lsp_implementations, { buf = args.buffer })
-		vim.keymap.set('n', 'gO', fzf.lsp_document_symbols, { buf = args.buffer })
+		vim.keymap.set('n', 'go', fzf.lsp_document_symbols, { buf = args.buffer })
 
 		vim.keymap.set('n', '<C-p>o', fzf.lsp_workspace_symbols, { buf = args.buffer })
 	end,
