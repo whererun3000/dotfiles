627cb4ab32a12339a6077a3e773d3be77f674ee1
whererun3000 <pookkljk151@gmail.com>
2026-02-12 01:12:25 +0800

    config/fish

---
+++ config/fish/conf.d/nvim.fish
@@ -0,0 +1,3 @@
+if status is-interactive
+    set -gx NVIM_APPNAME lazynvim
+end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,17 +1,2 @@
 if status is-interactive
-    set -gx EDITOR vim
-    set -gx VISUAL vim
-
-    # set -gx XDG_BIN_HOME "$HOME/.local/bin"
-    # set -gx XDG_DATA_HOME "$HOME/.local/share"
-    # set -gx XDG_STATE_HOME "$HOME/.local/state"
-    # set -gx XDG_CACHE_HOME "$HOME/.cache"
-    # set -gx XDG_CONFIG_HOME "$HOME/.config"
-
-    set -gx NVIM_APPNAME lazynvim
-
-    set -gx LESSKEYIN "$XDG_CONFIG_HOME/less/.lesskey"
-    set -gx LESSHISTFILE "$XDG_CACHE_HOME/less/.lesshst"
-
-    set -gx PATH $XDG_BIN_HOME $PATH
 end
