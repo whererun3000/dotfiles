157fd7f16283dd36cce0c075eb2fac157203cbf9
whererun3000 <pookkljk151@gmail.com>
2026-03-14 21:22:08 +0800

    config

--- config/niri/scripts/swayidle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-swayidle -w \
-    timeout 300 'swaylock -f' \
-    timeout 600  'niri msg action power-off-monitors' \
-    resume 'niri msg action power-on-monitors' \
-    timeout 1200 'systemctl suspend'
