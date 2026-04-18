4d9d0ca00a1e1353732bba3082f67ef5280947c8
whererun3000 <pookkljk151@gmail.com>
2026-04-18 23:32:02 +0800

    config/lazynvim

--- config/lazynvim/lua/config/keymaps.lua
+++ config/lazynvim/lua/config/keymaps.lua
@@ -2,5 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("n", "H")
-vim.keymap.del("n", "L")
+local map = LazyVim.safe_keymap_set
+
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
+map("c", "<C-b>", "<Left>")
+map("c", "<C-f>", "<Right>")
+map("c", "<C-a>", "<Home>")
+map("c", "<C-e>", "<End>")
+map("c", "<A-b>", "<C-Left>")
+map("c", "<A-f>", "<C-Right>")
