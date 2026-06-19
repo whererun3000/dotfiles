11cc4fbbc6ba474b40143a490f1b518d3c63a6b3
whyam <whyam@whyamdeMacBook-Pro.local>
2026-06-20 03:16:10 +0800

    config

--- config/fish/conf.d/3_brew.fish
+++ config/fish/conf.d/3_brew.fish
@@ -1,21 +1,24 @@
 if status is-login
-    switch (uname)
-        case Darwin
-            switch (uname -m)
-                case arm64
-                    set HOMEBREW_PREFIX /opt/homebrew
-                case '*'
-                    set HOMEBREW_PREFIX /usr/local/homebrew
-            end
+    switch (uname -m)
+        case arm64
+            set BREW_PATH /opt/homebrew
+        case '*'
+            set BREW_PATH /usr/local/homebrew
     end
 
-    if test -e "$HOMEBREW_PREFX"
-        eval $($HOMEBREW_PREFIX/bin/brew shellenv)
+    if test -e "$BREW_PATH"
+        eval $($BREW_PATH/bin/brew shellenv)
 
         set -gx HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
         set -gx HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
 
         set -gx HOMEBREW_API_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles/api"
         set -gx HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
+
+        set -l LLVM_PATH "$HOMEBREW_PREFIX/opt/llvm"
+
+        if test -e "$LLVM_PATH"
+            set -gxp PATH "$LLVM_PATH/bin"
+        end
     end
 end
--- config/nvim/lazy/lua/config/options.lua
+++ config/nvim/lazy/lua/config/options.lua
@@ -4,4 +4,6 @@
 
 if vim.g.neovide then
   vim.g.snacks_animate = false
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_input_macos_option_key_is_meta = "both"
 end
